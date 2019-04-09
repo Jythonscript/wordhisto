0acc858268f62c94217ba376fbe943fb384da6a9
Jythonscript <bartnik314@gmail.com>
2019-04-08 22:03:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wordhisto
+Make a histogram of words from a file. Useful for searching for overused words in essays.
---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+iterate through input file
+    get word
+    check if current word is in dictionary
+    if it is
+        increment counter by 1
+    else
+        create it with a counter of 1
+
+print dictionary in readable format
+"""
+
+
